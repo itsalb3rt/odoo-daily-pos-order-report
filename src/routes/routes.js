8e29b1f8cab439283cb3c1e38fdf320d26564899
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-10-21 19:36:44 -0400

    feat
    * Added settings route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import ordersRoutes from '../routes/Orders';
-// import userRoutes from '../routes/User';
-// import TaskRoutes from '../routes/Task';
+import settingsRoutes from '../routes/Settings';
 // Initialization
 let router = express.Router();
 
 // Routes
-// router.use('/users', userRoutes);
-// router.use('/tasks', TaskRoutes);
 router.use('/orders', ordersRoutes);
+router.use('/settings', settingsRoutes);
 
 export default router;
