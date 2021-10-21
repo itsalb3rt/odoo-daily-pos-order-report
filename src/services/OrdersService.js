a733a4347869a5cfb1c077cff5a92a10a5ef2d40
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2021-10-20 21:13:53 -0400

    feat
    * Added relation with partners on service

--- src/services/OrdersService.js
+++ src/services/OrdersService.js
@@ -1,13 +1,16 @@
 import POSOrder from '../models/pos_order';
-
+import ResPartner from '../models/res_partner';
 class OrdersService {
 
   static async getAll(params) {
     const { criterions } = params;
-    
+
     try {
-      
-      const { rows, count } = await POSOrder.findAndCountAll({...criterions});
+
+      const { rows, count } = await POSOrder.findAndCountAll({
+        ...criterions,
+        include: [{ model: ResPartner }]
+      });
 
       return { rows: rows, count: count };
       // eslint-disable-next-line no-unreachable
