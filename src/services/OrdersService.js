70f25ec6cca96998920552bb0182cc5f4c552567
Albert Hidalgo <alhidalgodev@gmail.com>
2022-12-12 21:51:43 -0400

    feat
    * Handle the include invoices to the general report

--- src/services/OrdersService.js
+++ src/services/OrdersService.js
@@ -1,17 +1,45 @@
 import POSOrder from '../models/pos_order';
 import ResPartner from '../models/res_partner';
+import accountInvoice from '../models/account_invoice';
 class OrdersService {
 
   static async getAll(params) {
-    const { criterions } = params;
+    const { criterions, includeInvoices } = params;
 
     try {
+      const includeInvoicesBoolean = includeInvoices === 'true';
 
-      const { rows, count } = await POSOrder.findAndCountAll({
+      let { rows, count } = await POSOrder.findAndCountAll({
         ...criterions,
         include: [{ model: ResPartner }]
       });
+      
+      if (includeInvoicesBoolean) {
+        const invoiceCriterions = {
+          ...criterions,
+          where: {
+            ...criterions.where,
+            create_date: criterions.where.date_order,
+          }
+        };
 
+        delete invoiceCriterions.where.date_order;
+        const accountInvoiceResults = await accountInvoice.findAndCountAll({
+          ...invoiceCriterions,
+          include: [{ model: ResPartner }]
+        });
+        let accountInvoices = JSON.parse(JSON.stringify(accountInvoiceResults.rows));
+        accountInvoices = accountInvoices.map(o => ({
+          ...o,
+          pos_reference: o.reference,
+          date_order: o.create_date,
+          ncf: o.reference
+        }));
+
+        rows = [...rows, ...accountInvoices];
+        count += accountInvoiceResults.count;
+
+      }
       // sort by pos_reference
       rows.sort((a, b) => {
         if (a.pos_reference > b.pos_reference) {
